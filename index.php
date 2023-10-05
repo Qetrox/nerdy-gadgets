a3dd7b097fcd26b72f152f18fc6e41c2ac6faba1
akmo1206 <145553945+akmo1206@users.noreply.github.com>
2023-10-05 20:29:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -85,7 +85,12 @@
         </div>
         <div class="over-ons">
             <h1>Wie zijn wij?</h1>
-            <p>Wij zjn Nerdy Gadgets. Wij bieden een fantastische selectie van gadgets aan. Onze asortiment van producten zijn bedoeld om jou de best nerd experience in Nederland te geven!  </p>
+            <p> Welkom bij Nerdy Gadgets!
+
+Stap binnen in de geeky hemel, waar tech en nerd-cultuur samenkomen om je zintuigen te prikkelen. Nerdy Gadgets is dé plek voor gadgetliefhebbers en popcultuur aficionado's. Hier vind je een onweerstaanbare mix van futuristische technologie en nostalgie uit je favoriete films, strips en games.
+Bij Nerdy Gadgets geloven we dat nerds de wereld veranderen, en we zijn er trots op deel uit te maken van deze revolutie. Duik in onze virtuele schappen en ontdek de nieuwste en meest onverwachte gadgets die je nerd-zintuigen zullen laten tintelen.
+
+Klaar om je passie naar een hoger niveau te tillen? Begin je ontdekkingstocht bij Nerdy Gadgets en laat je innerlijke geek stralen! </p>
         </div>
 
         
