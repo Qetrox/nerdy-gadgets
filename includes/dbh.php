712bbe1924d544693ef431b153696030298be1d3
tarikt <thegreattarikt@gmail.com>
2023-10-12 00:14:22 +0200

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost"; //"web0157.zxcs.nl"
+$servername = "web0157.zxcs.nl"; //"localhost"
 $username = "u127250p176374_nerdygadgets";
 $password = "NerdyGadgets2023#";
 $dbname = "u127250p176374_nerdygadgets";
