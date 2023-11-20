2e94da5e994f0ae0df2789d33ae99e4c1d5058ec
Thegreatarikt <ikbentariktufenk@gmail.com>
2023-11-20 13:11:29 +0100

    test

--- includes/dbh.php
+++ includes/dbh.php
@@ -1,10 +1,9 @@
 <<<<<<< HEAD
 <?php
-$servername = "web0157.zxcs.nl"; //"web0157.zxcs.nl"
-=======
-<?php // Database connection settings/variables
+ 
+// Database connection settings/variables
 $servername = "localhost"; //"web0157.zxcs.nl"
->>>>>>> 5427475ed578a04ba7985556554de142d677a1a8
+
 $username = "u127250p176374_nerdygadgets";
 $password = "NerdyGadgets2023#";
 $dbname = "u127250p176374_nerdygadgets";
