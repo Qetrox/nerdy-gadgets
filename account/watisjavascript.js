7b79ee87de349778d2f5de884bb55c17a4e95bfc
Thegreatarikt <ikbentariktufenk@gmail.com>
2023-11-24 12:27:34 +0100

    reuquired fields in js

--- account/watisjavascript.js
+++ account/watisjavascript.js
@@ -13,6 +13,62 @@ validation
     ])
 
 
+    .addField("#voornaam", [
+        {
+            rule: "required",
+        },
+    ])
+
+    .addField("#achternaam", [
+        {
+            rule: "required",
+        },
+    ])
+
+    .addField("#voornaam", [
+        {
+            rule: "required",
+        },
+    ])
+    .addField("#postcode", [
+        {
+            rule: "required",
+        },
+    ])
+
+    .addField("#straatnaam", [
+        {
+            rule: "required",
+        },
+    ])
+    .addField("#huisnummer", [
+        {
+            rule: "required",
+        },
+        {
+            rule:"integer",
+        }
+    ])
+    .addField("#voornaam", [
+        {
+            rule: "required",
+        },
+    ])
+    .addField("#plaats", [
+        {
+            rule: "required",
+        },
+    ])
+    .addField("#psw", [
+        {
+            rule: "required",
+        },
+        {
+            rule: "strongPassword",
+        }
+    ])
+
+
 .onSuccess((event) => {
     document.getElementById("registratie").submit();
 
