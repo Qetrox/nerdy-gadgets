cf259062a336e5853100a625e26a1728be840efe
Daan <145553717+SupahGuy@users.noreply.github.com>
2023-11-24 11:19:14 +0100

    Legaal tekst toegevoegd

--- legal/privacy/index.php
+++ legal/privacy/index.php
@@ -2,14 +2,55 @@
 <link rel="stylesheet" href="../legal.css">
 
 <main class="Main">
-<p>Uw privacy is belangrijk voor ons.</p>
-<div class="over-ons"></div>
-<h1>Wie zijn wij?</h1>
-<p> Welkom bij Nerdy Gadgets!
 
-    Stap binnen in de geeky hemel, waar tech en nerd-cultuur samenkomen om je zintuigen te prikkelen. Nerdy Gadgets is dé plek voor gadgetliefhebbers en popcultuur aficionado's. Hier vind je een onweerstaanbare mix van futuristische technologie en nostalgie uit je favoriete films, strips en games.
-    Bij Nerdy Gadgets geloven we dat nerds de wereld veranderen, en we zijn er trots op deel uit te maken van deze revolutie. Duik in onze virtuele schappen en ontdek de nieuwste en meest onverwachte gadgets die je nerd-zintuigen zullen laten tintelen.
+    <div class="CookiebeleidText">
+<p>
+    <h1 style="text-align: center; color: #FFFF00">Cookiebeleid voor Nerdy-Gadgets.nl</h1><br>
 
-    Klaar om je passie naar een hoger niveau te tillen? Begin je ontdekkingstocht bij Nerdy Gadgets en laat je innerlijke geek stralen! </p>
-</div>
+    <h2>Welkom bij Nerdy Gadgets!</h2><br>
+
+    <h4>Laatst bijgewerkt op: 24 nov 2023</h4><br>
+
+    Deze website maakt gebruik van cookies om de gebruikerservaring te verbeteren. In dit cookiebeleid leggen we uit wat cookies zijn,<br>
+    hoe we ze gebruiken op onze website en welke keuzes u hebt met betrekking tot deze cookies.<br><br><br>
+
+
+        <h2>Wat zijn cookies?</h2><br>
+        Cookies zijn kleine tekstbestanden die op uw apparaat worden geplaatst wanneer u een website bezoekt.<br>
+        Ze worden veelal gebruikt om websites efficiënter te laten werken,<br>
+        evenals om informatie te verstrekken aan de eigenaars van de website.<br><br><br>
+
+        <h2>Hoe gebruiken we cookies?</h2>
+
+        <h4>Nerdy Gadgets maakt gebruik van de volgende soorten cookies:</h4><br>
+
+        <h4>Strikt noodzakelijke cookies:</h4> Deze cookies zijn essentieel om u in staat te stellen door de website te navigeren en de functies ervan te gebruiken. Zonder deze cookies zouden bepaalde diensten niet beschikbaar zijn.<br>
+
+        <h4>Prestatiecookies:</h4> Deze cookies verzamelen informatie over hoe bezoekers de website gebruiken, zoals welke pagina's ze het vaakst bezoeken en of ze foutmeldingen van webpagina's ontvangen.<br>
+        Deze cookies worden gebruikt om de prestaties van de website te verbeteren<br>
+
+        <h4>Functionele cookies:</h4>  Deze cookies onthouden keuzes die u maakt en verbeteren zo uw gebruikservaring.
+        Ze kunnen bijvoorbeeld uw voorkeurstaal onthouden.
+        <h4>Analytische cookies:</h4>  We gebruiken tools van derden, zoals Google Analytics, om informatie te verzamelen over het gebruik van de website. Deze gegevens helpen ons de website te analyseren en te verbeteren.
+
+        <h4>Uw keuzes</h4>
+
+        Als u niet wilt dat wij cookies op uw apparaat plaatsen, kunt u uw browserinstellingen aanpassen om cookies te weigeren.<br>
+        Houd er rekening mee dat het uitschakelen van cookies de functionaliteit van sommige delen van de website kan beïnvloeden.<br><br>
+
+        <h4>Wijzigingen in dit cookiebeleid</h4>
+               --Geen wijzigingen--<br><br>
+
+       <h4> We behouden ons het recht voor om dit cookiebeleid op elk moment bij te werken.</h4><br>
+        Eventuele wijzigingen zullen onmiddellijk van kracht zijn zodra ze op de website zijn geplaatst.<br><br>
+
+        Bedankt voor uw bezoek aan Nerdy Gadgets!<br><br>
+        Als u vragen hebt over ons cookiebeleid, neem dan gerust contact met ons op via helpdesk@nerdy-gadgets.nl<br><br>
+
+        Laatst bijgewerkt op 24 november 2023
+        </p>
+    </div>
+
+
+    </div>
 </main>
