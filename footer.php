1ca4aab941ee2c33ce59fb15780d6a4d37a90996
Daan <145553717+SupahGuy@users.noreply.github.com>
2023-11-30 12:55:19 +0100

    footer verbeterd

--- footer.php
+++ footer.php
@@ -1,22 +1,22 @@
 <footer>
+    <div class ="footer">
+    <h4 style="margin-top: -2%;">@Copyright nerdy-gadgets.nl (Groep 1) 2023 - All Right Reserved.<br><br><span style="text-decoration: underline">DISCLAIMER: Dit is geen echte webshop, dit is een school project.</span></h4>
+    </div>
     <div class="footer-content">
+        </div>
         <!--dit maakt de tekst in de footer -Daan -->
         <div class="col1">
-            <h3 style="color:#21212F"; >Handige Links</h3>
-            <a style="color:#21212F"; href="https://www.Nerdy-Gadgets.nl/#about">Over ons</a>
-            <a style="color:#21212F"; href="https://www.Nerdy-Gadgets.nl/legal/privacy/">Legaal</a>
-            <a style="color:#21212F"; href="https://www.Nerdy-Gadgets.nl/search/">Producten</a>
-            <a style="color:#21212F"; href="https://www.Nerdy-Gadgets.nl/account/">Account</a>
-            <a style="color:#21212F"; href="https://www.Nerdy-Gadgets.nl/">Home</a>
+            <h4 >Handige Links</h4>
+            <a href="https://www.Nerdy-Gadgets.nl/#about">Over ons</a>
+            <a href="https://www.Nerdy-Gadgets.nl/legal/privacy/">Legaal</a>
+            <a href="https://www.Nerdy-Gadgets.nl/search/">Producten</a>
+            <a href="https://www.Nerdy-Gadgets.nl/account/">Account</a>
+            <a href="https://www.Nerdy-Gadgets.nl/">Home</a>
 
         </div>
         <div class="col2">
-            <h6 style="color:#21212F"; >@Copyright nerdy-gadgets.nl (Groep 1) 2023 - All Right Reserved.<br><span style="text-decoration: underline">DISCLAIMER: Dit is geen echte webshop, dit is een school project.</span></h6>
-
-        </div>
-        <div class="col3">
-            <h3 style="color:#21212F"; >CONTACT</h3>
-            <p style="color:#21212F"; >Hospitaaldreef 5,<br> 1315 RC Almere<br>Tel: +088 469 6600</p>
+            <h4>CONTACT</h4>
+            <p>Hospitaaldreef 5,<br> 1315 RC Almere<br>Tel: +088 469 6600</p>
         </div>
 
     </div>
