dd863f4ec3f96dc1a533881f5676c9add434b400
Farra2003 <145554104+Farra2003@users.noreply.github.com>
2023-12-04 12:59:54 +0100

    cookies

--- legal/privacy/index.php
+++ legal/privacy/index.php
@@ -12,7 +12,7 @@
     <body>
     <div class="CookiebeleidText">
         <p>
-        <h1 style="text-align: center; color: #FFFF00">Cookiebeleid voor Nerdy Gadgets</h1><br>
+        <h1 style="text-align: center; color: white">Cookiebeleid voor Nerdy Gadgets</h1><br>
 
 
     <link rel="stylesheet" href="../Legal.css">
@@ -20,7 +20,7 @@
     <main class="Main">
 <body>
     <div class="CookiebeleidText">
-<p>
+
     <h2 style="margin-left: -1em;">Cookiesbeleid</h2><br>
 
     <h4 style="margin-top: -1em">Laatst bijgewerkt op: 24 nov 2023</h4><br>
@@ -31,32 +31,33 @@
 
         <h2 style="margin-left: -1em;">Wat zijn cookies?</h2><br>
         Cookies zijn kleine tekstbestanden die op uw apparaat worden geplaatst wanneer u een website bezoekt.<br>
-        Ze worden veelal gebruikt om websites efficiënter te laten werken,<br>
+        Ze worden veelal gebruikt om websites efficiënter te laten werken,
         evenals om informatie te verstrekken aan de eigenaars van de website.<br><br><br><br>
 
         <h2 style="margin-left: -1em; margin-top: -0.5em">Hoe gebruiken we cookies?</h2>
 
         <h4>Nerdy Gadgets maakt gebruik van de volgende soorten cookies:</h4><br>
 
-        <h5 style="margin-left: -0.5em; margin-top: -0.5em;">Strikt noodzakelijke cookies:</h5> *
-        Deze cookies zijn essentieel om u in staat te stellen door de website te navigeren en de functies ervan te gebruiken.
-        Zonder deze cookies zouden bepaalde diensten niet beschikbaar zijn.<br><br>
+        <h5 style="margin-left: -0.5em; margin-top: -0.5em;">Strikt noodzakelijke cookies</h5>
+        <li> Deze cookies zijn essentieel om u in staat te stellen door de website te navigeren en de functies ervan te gebruiken.
+            Zonder deze cookies zouden bepaalde diensten niet beschikbaar zijn.<br><br>
 
-        <h5 style="margin-left: -0.5em;">Prestatiecookies:</h5>
-        * Deze cookies verzamelen informatie over hoe bezoekers de website gebruiken, zoals welke pagina's ze het vaakst bezoeken en of ze foutmeldingen van webpagina's ontvangen.<br>
-        Deze cookies worden gebruikt om de prestaties van de website te verbeteren<br><br>
+        <h5 style="margin-left: -0.5em;">Prestatiecookies</h5>
+        <li>Deze cookies verzamelen informatie over hoe bezoekers de website gebruiken, zoals welke pagina's ze het
+            vaakst bezoeken en of ze foutmeldingen van webpagina's ontvangen.<br>
+        <li>Deze cookies worden gebruikt om de prestaties van de website te verbeteren<br><br>
 
-        <h5 style="margin-left: -0.5em;">Functionele cookies:</h5>  * Deze cookies onthouden keuzes die u maakt en verbeteren zo uw gebruikservaring.
+        <h5 style="margin-left: -0.5em;">Functionele cookies</h5>  <li>Deze cookies onthouden keuzes die u maakt en verbeteren zo uw gebruikservaring.
         Ze kunnen bijvoorbeeld uw voorkeurstaal onthouden.<br><br>
 
-        <h4 style="margin-left: -0.5em;">Analytische cookies:</h4> * We gebruiken tools van derden, zoals Google Analytics, om informatie te verzamelen over het gebruik van de website.
+        <h4 style="margin-left: -0.5em;">Analytische cookies</h4> <li>We gebruiken tools van derden, zoals Google Analytics, om informatie te verzamelen over het gebruik van de website.
         Deze gegevens helpen ons de website te analyseren en te verbeteren.<br><br>
 
 
         <h4 style="margin-left: -1em;">Uw keuzes</h4>
 
-        * Als u niet wilt dat wij cookies op uw apparaat plaatsen, kunt u uw browserinstellingen aanpassen om cookies te weigeren.<br>
-        Houd er rekening mee dat het uitschakelen van cookies de functionaliteit van sommige delen van de website kan beïnvloeden.<br><br>
+        <li>Als u niet wilt dat wij cookies op uw apparaat plaatsen, kunt u uw browserinstellingen aanpassen om cookies te weigeren.<br>
+        <li>Houd er rekening mee dat het uitschakelen van cookies de functionaliteit van sommige delen van de website kan beïnvloeden.<br><br>
 
         <h4 style="margin-left: -1em;">Wijzigingen in dit cookiebeleid</h4>
 
