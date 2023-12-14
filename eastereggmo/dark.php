dcdc0f0fb864f4203ea414c0b5df6fb0d8da1ce6
akmo1206 <145553945+akmo1206@users.noreply.github.com>
2023-12-14 19:52:13 +0100

    gameroom
    gameroom

---
+++ eastereggmo/dark.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html lang="nl-nl" xmlns="http://www.w3.org/1999/html">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nerdy Gadgets</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <link rel="manifest" href="../favicon/site.webmanifest">
+    <link rel="mask-icon" href="../favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <script src="../index.js"></script>
+    <link rel="stylesheet" href="../base_stylesheet.css">
+    <link rel="stylesheet" href="../stylesheet.css">
+    <link rel="stylesheet" href="../load.css">
+</head>
+
+<div class="loaderscreen"></div>
+<body>
+<?php include_once '../header.php'?>
+<center><h2> Versla de highscore!</h2></center>
+<center><h1> Ben jij een echte nerd!</h1></center>
+<main>
+    <center><div><script src="https://cdn.htmlgames.com/embed.js?game=StayInTheDark&amp;bgcolor=white"></script></div></center>
+    <center> <h5>Stay in the dark and kill all the lights. Move around and hit the lights before you are exposed. </h5></center>
+</main>
+<?php include_once '../footer/footer.php' ?>
+</body>
+
+<script src="../typewriter.js"></script>
+
+</html>
