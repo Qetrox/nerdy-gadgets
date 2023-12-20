0505d891891f382366c45ca922972cbbc6b05f47
SupahGuy <DaanterWengel@gmail.com>
2023-12-20 20:30:16 +0100

    max 10 em height home button

--- SECRETLINK/SECRETLINK.php
+++ SECRETLINK/SECRETLINK.php
@@ -36,7 +36,7 @@
 <p style="text-align: center">  Dankjewel voor  uw bezoek aan Nerdy Gadgets </p>
 <div class="return-home">
 <a href="https://nerdy-gadgets.nl/"><button  style="width: 25em; height: 10em; background: #21212F; text-align: center; color: white; display: block;
-margin-left: auto; margin-right: auto; width: 20%; font-weight: bold; font-size: 300%">HOME</button></a>
+margin-left: auto; margin-right: auto; width: 20%; font-weight: bold; font-size: 300%; max-height: 10em">HOME</button></a>
 </div>
 
 </div>
